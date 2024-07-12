11bef40f3a670d674fcc8d4723b76af5e83f6151
Matthew Lucos <matthew.lucos@betterask.erni>
2024-07-12 22:43:49 +0800

    added a simple controller method

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+//using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
     // GET: /HelloWorld/
     public string Index()
     {
         return "This is my default action...";
     }
-    // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    // args must be explicitly specified as query params?? yes.
+    // ^ e.g. http://localhost:5117/HelloWorld/Welcome/?name=Matt&id=10
+    public string Welcome(string name, int id = 1)
     {
-        return "This is the Welcome action method...";
+        //return "This is the Welcome action method...";
+        //return HtmlEncoder.Default.Encode($"Hello: {name}, ID is {id}");
+        return $"Hello {name}, NumTimes is: {id}";
+        // you can make params optional with `?` at the end of them
     }
 }
+
+// cont. Add a View w/ VSCode
+// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-view?view=aspnetcore-8.0&tabs=visual-studio-code#add-a-view
